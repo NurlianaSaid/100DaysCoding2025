553e78812412f6782447837bb75292fbdf57bb6a
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-09-09 10:55:25 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,15 @@
+package liana;
+
+public class Main {
+    public static void main(String[] args) {
+        int tinggi =160;
+        String nama ="Nurliana";
+
+        System.out.println("Hai Semua ^°^ ") ; //untuk mencetak teks dengan baris baru
+        System.out.printf("Nama saya %s%n ", nama); //untuk mencetak teks dengan kode tertentu 
+        System.out.print("Tb saya "+ tinggi) ;  //untuk mencetak teks tanpa baris baru
+       
+    }
+    
+}
+    
