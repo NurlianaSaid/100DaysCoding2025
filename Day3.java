6db7ee30188b441394b3736c1e4818c79b7b5545
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-09-10 20:50:20 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,8 @@
+package Liana25;
+public class main1 {
+    public static void main(String[] args) {
+        System.out.println("Liana");
+        // ini adalah komentar yang hanya satu baris
+        /* dan ini adalah komentar lebih dari satu baris */
+    }
+}
