c70348bf13fb461333168e4e2a17a8db3613b6f8
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-09-11 23:08:49 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,29 @@
+package liana;
+
+public class Day4 {
+    public static void main(String[] args) {
+        // kode (\t) menambah spasi
+        System.out.println("nama :\tNurliana");
+        
+        // kode (\\) menambah garis miring terbalik pada output
+        System.out.println("contoh : \\");
+        
+        // kode (\n) memindahkan teks ke garis baru
+        System.out.println("Sedang \n belajar");
+        
+        // kode (\r) mengembalikan kursor ke awal baris
+        System.out.println("Harus \r Semangat");
+        
+        // kode (\b) menghapus karakter sebelumnya
+        System.out.println("Halo\b Dunia");
+        
+        // kode (\f) form feed, memindahkan ke halaman baru (tidak semua terminal mendukung)
+        System.out.println("Halaman 1\fHalaman 2");
+        
+        // kode (\") menambah tanda petik ganda dalam string
+        System.out.println("Dia berkata, \"Halo Dunia\"");
+        
+        // kode (\') menambah tanda petik tunggal dalam string
+        System.out.println("Dia berkata, \'Halo Dunia\'");
+    }
+}
