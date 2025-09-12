aede523d0dbe6218ecb172e87f0bf0c589aa93b0
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-09-12 21:42:40 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,17 @@
+package numerik;
+
+public class Day5 {
+   public static void main(String [] args) {
+
+System.out.println("Nota Harga Jualan yang Laku...");
+int harga_tas = 200000;
+short jumlah = 5;
+byte berat = 2;
+long total = (harga_tas)* jumlah;
+       System.out.println("Harga Tas = " + harga_tas);
+System.out.println("Berat Barang = " + berat +"Kg");
+System.out.println("Jumlah yang dibeli = "+jumlah);
+System.out.println(" Total Bayar Rp."+total);
+
+}
+}
