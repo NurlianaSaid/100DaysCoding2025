a858fba3f8004e5e1aa1ba2a92bd813ad37a238b
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-09-15 18:28:23 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,17 @@
+package nurliana;
+
+public class Day8 {
+    public static void main(String[] args){
+        String pertama = "Halo Semuanya...";
+        String kedua = "Semangat Terus Yah ^°^ ";
+        
+        System.out.println(pertama);
+        System.out.println(kedua);
+        
+        String one = "Jangan takut Error";
+        String two = "Tapi takutlah jika tak pernah mencoba";
+        
+        String both = one +" "+ two;
+        System.out.println(both);
+    }
+}
