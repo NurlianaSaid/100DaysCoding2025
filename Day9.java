cf47bd423ff49cab4280d5f1138d121fa59c647b
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-09-16 19:22:28 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,27 @@
+package liana;
+
+public class Day9 {
+    
+    public static void main(String[] args) {
+        //deklarasi variabel
+        String nama;
+        int nim;
+        char ipk;
+        
+        //inisialisasi variabel setelah deklarasi(pengisian nilai)
+        nama = "Nurlian";
+        nim = 22551;
+        ipk = '3';
+        
+        //update nilai variabel
+        nama = "Nurliana";
+        nim = 225514;
+        ipk = '4';
+        
+        
+        System.out.println("Nama Saya \t= \t"+nama);
+        System.out.println("Nim Saya \t= \t"+nim);
+        System.out.println("IPK Saya \t= \t"+ipk);
+    }
+    
+}
