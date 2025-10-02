c909d9b219ff09ccb27e98db645d9a8cbe8948bd
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-10-02 18:58:24 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class decrement {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Demonstrasi Pre-Increment dan Post-Increment
+        System.out.println("Demonstrasi Increment");
+        System.out.print("Masukkan nilai x = ");
+        int x = input.nextInt();
+        System.out.println("Nilai x awal = " + x);
+
+        int y = ++x; // Pre-Increment
+        System.out.println("Nilai y (pre-increment) = " + y);
+        System.out.println("Nilai x setelah pre-increment = " + x);
+
+       // x = input.nextInt(); // Reset nilai x
+        System.out.print("Masukkan Nilai X = ");
+        System.out.println("Nilai x awal = " + x);
+
+        int z = x++; // Post-Increment
+        System.out.println("Nilai z (post-increment) = " + z);
+        System.out.println("Nilai x setelah post-increment = " + x);
+
+        // Demonstrasi Pre-Decrement dan Post-Decrement
+        System.out.println("\nDemonstrasi Decrement");
+        System.out.print("Masukkan nilai a = ");
+        int a = input.nextInt();
+        System.out.println("Nilai a awal = " + a);
+
+        int b = --a; // Pre-Decrement
+        System.out.println("Nilai b (pre-decrement) = " + b);
+        System.out.println("Nilai a setelah pre-decrement = " + a);
+
+        //a = input.nextInt(); // Reset nilai a
+        System.out.println("Nilai a awal = " + a);
+
+        int c = a--; // Post-Decrement
+        System.out.println("Nilai c (post-decrement) = " + c);
+        System.out.println("Nilai a setelah post-decrement = " + a);
+    }
+}
