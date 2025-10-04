2ad43518ea7a31ecb42f1d3b7d7a44364f67d183
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-10-04 20:24:45 +0800

    Create Day27.java

---
+++ Day27.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class lianaku {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Nama : ");
+        String a = in.nextLine();
+        System.out.print("Umur : ");
+        int b = in.nextInt();
+        System.out.println("Nama saya adalah: "+a);
+        System.out.print("Umur saya adalah : "+b+"\n");
+        System.out.printf("Umur anda %d tahun",(b+5));
+    }
+}
