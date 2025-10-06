feb8ccf0a0bd7d16b3264562e6d137951764fbc7
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-10-06 22:18:18 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,14 @@
+public class Day29 {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        
+        if(a>b){
+            System.out.println("Apa ia?");
+        }else if(a<b){
+            System.out.println("Sipp");
+        }
+    }
+    
+}
