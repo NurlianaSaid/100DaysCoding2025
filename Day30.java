4e2ecb1dc99591822872828efd0aaaa2b6f15beb
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-10-07 20:08:20 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,13 @@
+public class Day29 {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 11;
+         if(a>=b){
+            System.out.println("A lebih besar sama dengan B");
+        }else if(a<=b){
+            System.out.println("A lebih kecil sama dengan B");
+        }
+    }
+    
+}
