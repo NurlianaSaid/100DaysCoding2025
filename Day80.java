00ad1d0c21d941930950a85d55509fd28edceaa7
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-11-26 23:29:26 +0800

    Create Day80.java

---
+++ Day80.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Arra {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int[] a = {1,2,3};
+        
+        for(int b : a) System.out.print(b +" ");
+    }
+    
+}
