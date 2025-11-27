55aa6053cded629dcb8abe83de0bd60292b139fd
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-11-27 22:03:45 +0800

    Create Day81.java

---
+++ Day81.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+public class soal2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] a = {"liana","ana","lina","nur","nana"};
+        System.out.println(a[0]+" "+a[1]+" "+a[2]+" "+a[3]+" "+a[4]+" ");
+    }
+}
