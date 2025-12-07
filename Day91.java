468087ef66f37f1bef4ed64a3b64c412eb4445bf
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-12-07 23:35:42 +0800

    Create Day91.java

---
+++ Day91.java
@@ -0,0 +1,9 @@
+
+public class NonVoid {
+    static int kali(int a, int b){ //a & b adalah parameter method
+        return a*b;
+    }
+    public static void main(String[] args) {
+       System.out.print(kali(4,6));
+    }
+}
